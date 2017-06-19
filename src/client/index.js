a14a17e02685efe7659e27a601eb1fd9da9abb98
Michal Kvasničák <michal.kvasnicak@gmail.com>
2017-06-19 12:21:38 +0200

    ⬆️ update spust to 0.11.0 and remove offline-plugin because it is optional

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,10 @@
 // @flow
 
-import * as OfflinePluginRuntime from 'offline-plugin/runtime';
-
 // src/client/index.js
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from '../shared/App';
-
-OfflinePluginRuntime.install();
 
 render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('app'));
