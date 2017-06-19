45af9b783c2559dae5031a7dcd8763266d7e8143
Michal Kvasničák <michal.kvasnicak@gmail.com>
2017-06-19 11:38:14 +0200

    ✨ add static files serving for production

--- src/server/index.js
+++ src/server/index.js
@@ -3,14 +3,28 @@
 // src/server/index.js
 import micro, { send } from 'micro';
 import React from 'react';
+import staticServerHandler from 'serve/lib/server';
 import { assets } from 'spust';
 import { renderToString } from 'react-dom/server';
 import type { StaticRouterContext } from 'react-router';
 import { StaticRouter } from 'react-router-dom';
+import { parse } from 'url';
 
 import App from '../shared/App';
 
+// ASSET_DIR is used by serve's server handler
+// this is coppied from server/bin/serve.js
+process.env.ASSET_DIR = '/' + Math.random().toString(36).substr(2, 10);
+
 const server = micro(async (req, res) => {
+  const url = parse(req.url);
+
+  // try to serve only files with extensions *.*
+  // this is just a hack because I haven't found nice static files serving middleware for micro
+  if (url.pathname && url.pathname.match(/\..+$/)) {
+    return await staticServerHandler(req, res, {}, process.env.ASSETS_PATH);
+  }
+
   const { css, js } = assets();
 
   const context: StaticRouterContext = {};
@@ -39,6 +53,6 @@ const server = micro(async (req, res) => {
 });
 
 // default port is 3000 provided by Spust in dev mode
-server.listen(process.env.PORT);
+server.listen(process.env.PORT || 3000);
 
 export default server;
